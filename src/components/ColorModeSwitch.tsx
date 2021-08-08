dc16959eb50c75c9ee91775af265bd6e78b4fb3c
Tom Buhl-Nielsen <tombuhlnielsen@gmail.com>
2021-08-08 19:54:18 +0100

    Fix color mode icon

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import Brightness4Icon from '@material-ui/icons/Brightness4'
-import Brightness7Icon from '@material-ui/icons/Brightness4'
+import Brightness7Icon from '@material-ui/icons/Brightness7'
 import Tooltip from '@material-ui/core/Tooltip'
 import { useGlobalState } from '../contexts'
 
@@ -25,7 +25,8 @@ const ColorModeSwitch = () => {
     <div style={styles}>
       <Tooltip title={label} arrow>
         <IconButton onClick={handleClick} aria-label={label}>
-          {colorMode === 'light' ? <Brightness4Icon /> : <Brightness7Icon />}
+          {colorMode === 'light' && <Brightness4Icon />}
+          {colorMode === 'dark' && <Brightness7Icon />}
         </IconButton>
       </Tooltip>
     </div>
