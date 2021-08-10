ea1212f443778badc2fea7c044ddfaea6649457d
Tom Buhl-Nielsen <tombuhlnielsen@gmail.com>
2021-08-10 13:09:35 +0100

    Move styles in line

--- src/components/EditableTodoList.tsx
+++ src/components/EditableTodoList.tsx
@@ -3,15 +3,11 @@ import FilterButtonGroup from './FilterButtonGroup'
 import NewItemForm from './NewItemForm'
 import TodoList from './TodoList'
 
-const formStyles = {
-  marginTop: '24px'
-}
-
 const EditableTodoList = () => {
   return (
     <>
       <FilterButtonGroup />
-      <div style={formStyles}>
+      <div style={{ marginTop: '24px' }}>
         <NewItemForm />
         <TodoList />
       </div>
